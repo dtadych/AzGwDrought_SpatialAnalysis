763f138ad7bdbeae3f2c409093fcc120c5d00129
Danielle Tadych <60152156+dtadych@users.noreply.github.com>
2021-06-01 17:51:19 -0700

    Cleaning up the code

--- SummaryCodes/Wells55_GWSI_Merge.py
+++ SummaryCodes/Wells55_GWSI_Merge.py
@@ -17,39 +17,6 @@ import geopandas as gp
 
 # %% 
 # ----- Import the Data and Shapefiles with Geometries -----
-# Read in Wells 55 Data
-# This is a file with water levels from ADWR which has been joined with another ADWR file with variables
-filename = 'Wells55.csv'
-datapath = '../MergedData'
-outputpath = '../MergedData/Output_files'
-filepath = os.path.join(datapath, filename)
-print(filepath)
-
-wells55 = pd.read_csv(filepath)
-pd.options.display.float_format = '{:.2f}'.format
-print(wells55.info())
-# %%
-# Read in pump_wl 
-# This is a combined file with pump data & depth to water
-filename = 'Pump_wl.csv'
-filepath = os.path.join(datapath, filename)
-print(filepath)
-
-pump_wl = pd.read_csv(filepath)
-pd.options.display.float_format = '{:.2f}'.format
-print(pump_wl.info())
-
-# %%
-# Read in GWSI merged water level data
-filename = 'wl_data2.csv'
-filepath = os.path.join(datapath, filename)
-print(filepath)
-
-wl_data2 = pd.read_csv(filepath)
-pd.options.display.float_format = '{:.2f}'.format
-print(wl_data2.info())
-#%%
-# Now read in the shapefiles for both
 shapedir = '../MergedData/Shapefiles'
 #wellfilename = "Well_Registry__Wells55_.shp"
 wellfilename = "Well_Registry__Wells55_.shp"
@@ -76,24 +43,6 @@ plt.legend()
 # %%
 # === Merging geodatabases and shapefiles ===
 #  - according to stack overflow https://gis.stackexchange.com/questions/349244/merging-a-geodataframe-and-pandas-dataframe-based-on-a-column
-
-# reformatting registry ID's
-#wells55shape['REGISTRY_I'] = wells55shape['REGISTRY_I'].astype(int, errors = 'raise')
-#wells55_df = wells55shape.merge(wells55, on='REGISTRY_I', how = 'left')
-#wells55_gdf = gp.GeoDataFrame(wells55_df)
-#wells55_gdf.info()
-# %% This is not necessary for this database
-# Rename wellid in pumpwl to SITE_ID
-#pump_wl = pump_wl.rename(columns = {'wellid':'SITE_ID'}, errors = "raise")
-
-# Convert types 
-#pump_wl['SITE_ID'] = pump_wl['SITE_ID'].astype(object, errors = 'raise')
-#pump_wl.info()
-
-# Merge shape with database
-#gwsi_df = GWSIshape.merge(pump_wl, on="SITE_ID", how = 'left')
-#gwsi_gdf = gp.GeoDataFrame(gwsi_df)
-#gwsi_gdf.info()
 
 # %% Making copies of the databases so I don't overright the originals
 gwsi_gdf = GWSIshape
@@ -134,4 +83,3 @@ plt.savefig('../MergedData/Output_files/{0}.png'.format(type), bbox_inches='tigh
 # Export all the ish
 Wells55_GWSI_MasterDB.to_file("Master_ADWR_Database.shp")
 Wells55_GWSI_MasterDB.to_csv('../MergedData/Output_files/Master_ADWR_database.csv')
-# %%
